f5df75a02dc2c20afb5c786b060ab4609e1a8cc7
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-04 04:24:54 +0500

    ToolBarButton added

--- PersonalBlog/Blog/Controllers/BlogController.cs
+++ PersonalBlog/Blog/Controllers/BlogController.cs
@@ -4,9 +4,22 @@ namespace Blog.Controllers
 {
     public class BlogController : Controller
     {
+        static List<string> Posts = new List<string>();
+        public IActionResult Index()
+        {
+            return View("Index",Posts);
+        }
+        
         public IActionResult CreatorPage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatorPage(string content)
+        {
+            Posts.Add(content);
+            return RedirectToAction("Index");
         }
     }
 }
