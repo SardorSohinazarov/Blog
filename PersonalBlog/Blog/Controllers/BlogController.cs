445dca333cd4f1265340cd50367475569d9f9e47
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-04 04:46:17 +0500

    BlogEntry added

--- PersonalBlog/Blog/Controllers/BlogController.cs
+++ PersonalBlog/Blog/Controllers/BlogController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Blog.Models;
+using System.Linq;
 
 namespace Blog.Controllers
 {
     public class BlogController : Controller
     {
-        static List<string> Posts = new List<string>();
+        static List<BlogEntry> Posts = new List<BlogEntry>();
         public IActionResult Index()
         {
             return View("Index",Posts);
         }
         
-        public IActionResult CreatorPage()
+        public IActionResult CreatorPage(Guid id)
         {
+            if(id != Guid.Empty)
+            {
+                BlogEntry ExistingEntry = Posts.FirstOrDefault(x => x.Id == id);
+                return View(model:ExistingEntry);
+            }
             return View();
         }
 
---
+++ PersonalBlog/Blog/Models/BlogEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Models
+{
+    public class BlogEntry
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+    }
+}
