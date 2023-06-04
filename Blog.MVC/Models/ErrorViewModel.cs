5016d77bc985db0ece3adab6a6c0d76e0ea67970
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-06-04 15:42:09 +0500

    Added MVC projects

---
+++ Blog.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
